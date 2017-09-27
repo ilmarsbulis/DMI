7a0420ebf6dd41661077467fef9d14e675aa9fe7
Live session user <ilmarsbulis@gmail.com>
2017-09-27 08:47:31 +0000

    20178927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,14 +1,29 @@
 
 #!/bin/bash
+#4.piemers
+echo "Tiek pildits skripts:"$0
+#echo $0
+#echo $n
+echo "scriptm nodoto argumentu skaits" $#
+echo "scriptam nodotie argumenti (attelosana/grupesana 1):"$*
+echo "scriptam nodotie argumenti (attelosana/grupesana 2):"$@
+#echo $?
+echo "scriptam pieskirtajs processa numurs"$$
+echo $!
 
-NAME="Vaards Uzvaards"
 
+
+#3. piemers
+#NAME="Vaards Uzvaards"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#2. piemers
 #readonly NAME 
 #echo $NAME
 #NAME="Uzvaards Vaards"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
-
+#1. piemers
+#echo $NAME
