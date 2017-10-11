55aa917012c332c5ac9aa0c984ca1259104ec7d4
Live session user <ilmarsbulis@gmail.com>
2017-10-11 06:54:31 +0000

    20171011_9_50

---
+++ shell.operators.sh
@@ -0,0 +1,36 @@
+#3.oiemers - ar mainigiem 
+a=29
+b=15
+val31=`expr $a + $b`
+echo "$a + $b = "$val31
+val32=`expr $a - $b`
+echo "$a - $b = "$val32
+val33=`expr $a \* $b`
+echo "$a \* $b = "$val33
+val34=`expr $a / $b`
+echo "$a / $b = "$val34
+val35=`expr $a % $b`
+echo "$a % $b = "$val35
+
+
+
+
+
+# 2.piemers operacijas ar konstantiem
+: <<'END'
+val21=`expr 2 + 3`
+echo "2 + 3 = "$val21
+val22=`expr 2 - 3`
+echo "2 - 3 = "$val22
+val23=`expr 2 \* 3`
+echo "2 \* 3 = "$val23
+val24=`expr 2 / 3`
+echo "2 / 3 = "$val24
+val25=`expr 2 % 3`
+echo "2 % 3 = "$val25
+END
+
+
+
+
+
