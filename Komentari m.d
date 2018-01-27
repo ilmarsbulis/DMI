49fd328864f1b53ef99ff3c137221f155acfa4cf
ilmarsbulis <31915331+ilmarsbulis@users.noreply.github.com>
2018-01-27 13:32:57 +0200

    Create Komentari m.d

---
+++ Komentari m.d
@@ -0,0 +1,32 @@
+cat: lasit failu
+cd: parvietošanai starp direktorijam (pec cd rakstam direktorijas nosaukumu)
+cd ~ : atgriezties u majam
+cd . : stavet uz vietas
+cd .. : atgrieztes apakšmape 
+chmod: maina noteikta faila privekegijas (chmod 744 "faila nosaukums")
+echo: teksta radišanai 
+echo 0$: shell? 
+firefox&: atvert firfox brauzeri 
+git ... darbibas ar githabu
+history: terminala veikto darbibu apskats
+ls ; ls -la ; ls -a: 3 komandas lai apskatit failus, mapes, apakšamapes)
+lsb_release: rada svarigu informaciju
+mdkir: failu/direktorija izveidošana ( mdkir "jauna faila nosaukums")
+man: lai atvert noteiktas komandas manualu
+nano: skriptu veidošana un faila modificešana 
+path: izveidot/radit celu 
+pwd: noskaidrot kura direktorija pašlaik atrodos
+rmdir: noteikta faila/direktorija dzešana ( rmdir "faila nosaukums")
+uname ; uname -r: uzzinat informaciju par sistemu
+who ; woami: uzzinat profilu, kurš pašlaik lieto terminalu/sistema 
+
+
+
+*** papild ***
+
+tab - palidziba
+bultinas ^ un uz leju : iepriekš iavadito komandu parskats 
+bultinas < > - virzieni
+ctrl + l : nodziest visu 
+ctrl + T : atvert jaubo terminalu
+ctrl+k ; ctrl + l : darbs ar rindu slepšanu, kopešanu un ielimešanu (nano)
